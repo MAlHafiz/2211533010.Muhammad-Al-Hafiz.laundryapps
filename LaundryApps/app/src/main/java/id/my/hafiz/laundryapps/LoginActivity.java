9a69aea892d75833f68799dd24b85a4c95216d06
MAlHafiz <145088498+MAlHafiz@users.noreply.github.com>
2024-09-28 14:46:48 +0700

    Tugas Pekan 2
    Second commit

--- LaundryApps/app/src/main/java/id/my/hafiz/laundryapps/LoginActivity.java
+++ LaundryApps/app/src/main/java/id/my/hafiz/laundryapps/LoginActivity.java
@@ -1,6 +1,13 @@
 package id.my.hafiz.laundryapps;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +16,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edUsername, edPassword;
+    Button btnLogin;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,31 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Kenalkan widget didalam method create
+        edUsername = (EditText) findViewById(R.id.edUsername);
+        edPassword = (EditText) findViewById(R.id.edPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        // event handle ketika tombol login di klik
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(edUsername.getText().toString())){
+                    Toast.makeText(LoginActivity.this, "Username tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(edPassword.getText().toString())) {
+                    Toast.makeText(LoginActivity.this, "Password tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Loginkeun Cuy", Toast.LENGTH_LONG).show();
+                    // intent ketika di klik login akan pindah ke halaman main activity
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    // kirim data username ke activity main menggunakan intent
+                    intent.putExtra("username",edUsername.getText().toString());
+                    startActivity(intent);
+                    // setelah tombol login diklik maka activity login kita matikan
+                    finish();
+                }
+            }
+        });
+
     }
 }
